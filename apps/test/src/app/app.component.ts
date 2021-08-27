6f8df4ecb679ef1e9e3941070f11746a110d49d7
wrslatz <20246633+wrslatz@users.noreply.github.com>
2021-08-27 16:55:42 -0400

    initial commit

---
+++ apps/test/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-selective-hash-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'test';
+}
